1fccf9ac59192ac52be5f6352840affc51e35679
Rhys Evans <wheresrhys@gmail.com>
2015-11-11 22:11:15 +0000

    the beginnings of an isomorphic app

--- server/app.js
+++ server/app.js
@@ -1,3 +1,40 @@
+'use strict';
+
 const PouchDB = require('PouchDB');
 
+const app = require('koa')();
+const router = require('koa-router')();
 const db = new PouchDB(process.env.POUCHDB_HOST);
+
+// const routeConfig =
+const routeConfig = require('../shared/route-config');
+
+const controllers = {
+	root: function *(next) {
+	  this.body = 'Hello World: ' + Object.keys(this.params).map(k => k + ':' + this.params[k]).join(' ');
+	},
+	learn: function *(next) {
+	  this.body = 'learn: ' + Object.keys(this.params).map(k => k + ':' + this.params[k]).join(' ');
+	},
+	rehearse: function *(next) {
+	  this.body = 'rehearse: ' + Object.keys(this.params).map(k => k + ':' + this.params[k]).join(' ');
+	},
+	tunes: function *(next) {
+	  this.body = 'tunes: ' + Object.keys(this.params).map(k => k + ':' + this.params[k]).join(' ');
+	},
+	sets: function *(next) {
+	  this.body = 'sets: ' + Object.keys(this.params).map(k => k + ':' + this.params[k]).join(' ');
+	}
+};
+
+for(let name in routeConfig) {
+	routeConfig[name].forEach(pattern => {
+		router.get(pattern, controllers[name]);
+	});
+}
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000);
---
+++ shared/route-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	root: ['/'],
+	learn: ['/learn', '/learn/:tunebook'],
+	rehearse: ['/rehearse', '/rehearse/:tunebook'],
+	tunes: ['/tunes', '/tunes/:action/:tuneId'],
+	sets: ['/sets', '/sets/:action/:tuneId']
+};
